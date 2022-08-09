d5fd561ce33e4d603d3c34d9ea82132a5e226283
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-08-08 21:28:50 -0300

    resolviendo lo de imagenes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const resolve = require('path');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname,'./public');
-app.use(express.static(publicPath));
+
+app.use(express.static(path.join(__dirname,'./public')));
 
 app.listen(3000,() => { console.log('Servidor corriendo, puerto 3000');});
 
