ebb1b1f435dca55ecb6163815af3e15653869148
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-08-22 21:57:55 -0300

    ej de clase formularios

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(3000,() => { console.log('Servidor corriendo, puerto 3000');});
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname,'./views/home.html'))
 } );
+
+app.get('/registro', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'./views/register.html'))
+} );
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'./views/login.html'))
+} );
