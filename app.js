adb5122d3be57ad586bbc09e6548332adf19b898
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-08-29 19:54:50 -0300

    heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'./public')));
 
-app.listen(3000,() => { console.log('Servidor corriendo, puerto 3000');});
+app.listen(process.env.PORT || 3000,() => { console.log('Servidor corriendo, puerto 3000');});
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname,'./views/home.html'))
